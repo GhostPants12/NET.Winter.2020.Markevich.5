ee467b9e67023eae107b5c6c76e323a42f2a4db9
GhostPants12 <54960078+GhostPants12@users.noreply.github.com>
2020-01-29 16:48:06 +0300

    fix: remove unnecessary functionality

--- NumbersExtension/NumbersExtension.cs
+++ NumbersExtension/NumbersExtension.cs
@@ -7,68 +7,6 @@ namespace NumbersExtension
     /// </summary>
     public static class NumbersExtension
     {
-        public const double Epsilon = 1;
-        private const int IntMaxBit = (sizeof(int) * 8) - 1;
-        private const int IntMinBit = 0;
-
-        /// <summary>Inserts the number into another.</summary>
-        /// <param name="numberSource">The number source.</param>
-        /// <param name="numberIn">The number in.</param>
-        /// <param name="i">The i.</param>
-        /// <param name="j">The j.</param>
-        /// <returns>numberSource with inserted numberIn.</returns>
-        /// <exception cref="ArgumentOutOfRangeException">i - i is less than zero
-        /// or
-        /// j - j is less than zero
-        /// or
-        /// i - i is higher than number of bits in int
-        /// or
-        /// j - j is higher than number of bits in int.</exception>
-        /// <exception cref="ArgumentException">i is higher than j.</exception>
-        public static int InsertNumberIntoAnother(int numberSource, int numberIn, int i, int j)
-        {
-            if (i < IntMinBit || i > IntMaxBit)
-            {
-                throw new ArgumentOutOfRangeException(nameof(i), "i is out of range");
-            }
-
-            if (j < IntMinBit || j > IntMaxBit)
-            {
-                throw new ArgumentOutOfRangeException(nameof(j), "j is out of range");
-            }
-
-            if (i > j)
-            {
-                throw new ArgumentException("i is higher than j");
-            }
-
-            if (i == IntMinBit && j == IntMaxBit)
-            {
-                return numberIn;
-            }
-
-            if (j == IntMaxBit)
-            {
-                numberSource &= int.MaxValue;
-            }
-
-            int bitMaskForNumberIn, bitMaskForNumberSource;
-            bitMaskForNumberIn = ~(int.MaxValue << (j + 1 - i)) & int.MaxValue;
-            int insertValue = (numberIn & bitMaskForNumberIn) << i;
-            if (numberSource >= 0)
-            {
-                bitMaskForNumberSource = (int.MaxValue >> (IntMaxBit - i)) | ((int.MaxValue << (j + 1)) & int.MaxValue);
-            }
-            else
-            {
-                bitMaskForNumberSource = (int.MaxValue >> (IntMaxBit - i)) | (((int.MaxValue << (j + 1)) & int.MaxValue) | (int.MaxValue << (IntMaxBit - 1)));
-            }
-
-            int putOutValue = numberSource & bitMaskForNumberSource;
-            putOutValue |= insertValue;
-            return putOutValue;
-        }
-
         /// <summary>Determines whether the specified value is palindrome.</summary>
         /// <param name="value">The value.</param>
         /// <returns>
@@ -127,43 +65,6 @@ namespace NumbersExtension
             {
                 return false;
             }
-        }
-
-        /// <summary>Finds the NTH root.</summary>
-        /// <param name="value">The value.</param>
-        /// <param name="rootPower">The root power.</param>
-        /// <param name="accuracy">The accuracy.</param>
-        /// <returns>Returns the root of the value.</returns>
-        /// <exception cref="ArgumentException">Incorrect rootPower
-        /// or
-        /// number
-        /// or accuracy.</exception>
-        public static double FindNthRoot(double value, int rootPower, double accuracy)
-        {
-            if (rootPower <= 0)
-            {
-                throw new ArgumentOutOfRangeException("RootPower is less than zero");
-            }
-
-            if (value == 0 || ((value < 0) && (rootPower % 2 == 0)))
-            {
-                throw new ArgumentException("Incorrect number");
-            }
-
-            if (accuracy <= 0 || accuracy >= Epsilon)
-            {
-                throw new ArgumentOutOfRangeException("Accuracy is less than zero or higher than Epsilon");
-            }
-
-            double prev = value / rootPower;
-            double current = (1.0 / rootPower) * (((rootPower - 1) * prev) + (value / Math.Pow(prev, rootPower - 1)));
-            while (Math.Abs(current - prev) > accuracy)
-            {
-                prev = current;
-                current = (1.0 / rootPower) * (((rootPower - 1) * prev) + (value / Math.Pow(prev, rootPower - 1)));
-            }
-
-            return current;
         }
     }
 }
